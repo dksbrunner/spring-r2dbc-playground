4e1cd3f80d992530fb176b27928081bec9e2db2a
Daniel Brunner <dksbrunner@users.noreply.github.com>
2021-12-19 22:05:13 +0100

    fix implementation of `Persistable#isNew`

--- src/test/java/de/dksbrunner/persistence/model/EntityTest.java
+++ src/test/java/de/dksbrunner/persistence/model/EntityTest.java
@@ -15,15 +15,32 @@
  */
 package de.dksbrunner.persistence.model;
 
-import org.springframework.data.domain.Persistable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Daniel Brunner
  */
-public interface Entity extends Persistable<Long> {
+@ExtendWith(MockitoExtension.class)
+class EntityTest {
 
-    @Override
-    default boolean isNew() {
-        return getId() == null;
+    @Spy
+    private Entity cut;
+
+    @Test
+    void testIsNewReturnTrue() {
+        when(cut.getId()).thenReturn(null);
+        assertThat(cut.isNew()).isTrue();
+    }
+
+    @Test
+    void testIsNewReturnFalse() {
+        when(cut.getId()).thenReturn(1000L);
+        assertThat(cut.isNew()).isFalse();
     }
 }
